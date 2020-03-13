999f6afa590d062ab512278b5b5b91944e859471
Keal Jones <keal.jones@workiva.com>
2020-03-13 09:27:24 -0700

    re-add testing stuff

--- src/lib/baymax.ts
+++ src/lib/baymax.ts
@@ -1,4 +1,4 @@
-import { App } from '@slack/bolt';
+import { App, BlockAction } from '@slack/bolt';
 import { addChannelFunnel, getChannelsFor } from './store';
 
 /**
@@ -29,11 +29,136 @@ class BaymaxBot {
       say(`${JSON.stringify(getChannelsFor(toChannel))}`);
     });
 
-    this.boltClient.command('/what', async ({ command, ack, say }) => {
-      // Acknowledge command request
+    this.boltClient.action<BlockAction>(/level[0-9]/, async ({ body, ack, respond }) => {
+      // Acknowledge the action
       ack();
+      respond(`<@${body.user.id}> clicked: ${body.actions[0].action_id}`);
+    });
 
-      say(`idk`);
+    // Listens to incoming messages that contain "hello"
+    this.boltClient.message('ow', async ({ event, message, context }) => {
+      try {
+        const result = await this.boltClient.client.chat.postEphemeral({
+          token: context.botToken,
+          channel: event.channel,
+          user: event.user,
+          text: '',
+          blocks: [
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: `<@${message.user}>, I was alerted that you need medical attention when you said, "ow".\nOn a scale of 1 to ten, how would you rate your pain?!`
+              }
+            },
+            {
+              type: "actions",
+              elements: [
+                {
+                  type: "button",
+                  text: {
+                    type: "plain_text",
+                    text: ":grinning:",
+                    emoji: true
+                  },
+                  action_id: "level1",
+                },
+                {
+                  type: "button",
+                  text: {
+                    type: "plain_text",
+                    text: ":yum:",
+                    emoji: true
+                  },
+                  action_id: "level2",
+                },
+                {
+                  type: "button",
+                  text: {
+                    type: "plain_text",
+                    text: ":slightly_smiling_face:",
+                    emoji: true
+                  },
+                  action_id: "level3",
+                },
+                {
+                  type: "button",
+                  text: {
+                    type: "plain_text",
+                    text: ":neutral_face:",
+                    emoji: true
+                  },
+                  action_id: "level4",
+                },
+                {
+                  type: "button",
+                  text: {
+                    type: "plain_text",
+                    text: ":no_mouth:",
+                    emoji: true
+                  },
+                  action_id: "level5",
+                },
+              ]
+            },
+            {
+              type: "actions",
+              elements: [
+                {
+                  type: "button",
+                  text: {
+                    type: "plain_text",
+                    text: ":slightly_frowning_face:",
+                    emoji: true
+                  },
+                  action_id: "level6",
+                },
+                {
+                  type: "button",
+                  text: {
+                    type: "plain_text",
+                    text: ":white_frowning_face:",
+                    emoji: true
+                  },
+                  action_id: "level7",
+                },
+                {
+                  type: "button",
+                  text: {
+                    type: "plain_text",
+                    text: ":anguished:",
+                    emoji: true
+                  },
+                  action_id: "level8",
+                },
+                {
+                  type: "button",
+                  text: {
+                    type: "plain_text",
+                    text: ":tired_face:",
+                    emoji: true
+                  },
+                  action_id: "level9",
+                },
+                {
+                  type: "button",
+                  text: {
+                    type: "plain_text",
+                    text: ":face_with_symbols_on_mouth:",
+                    emoji: true
+                  },
+                  action_id: "level10",
+                },
+              ]
+            }]
+        });
+        // tslint:disable-next-line: no-console
+        console.log(result);
+      }
+      catch (error) {
+        // tslint:disable-next-line: no-console
+        console.error(error);
+      }
     });
   }
 
@@ -48,139 +173,4 @@ class BaymaxBot {
   }
 }
 
-
-// this.boltClient.action<BlockAction>(/level[0-9]/, async ({ body, ack, respond }) => {
-//   // Acknowledge the action
-//   ack();
-//   respond(`<@${body.user.id}> clicked: ${body.actions[0].action_id}`);
-// });
-
-// // Listens to incoming messages that contain "hello"
-// this.boltClient.message('ow', async ({ event, message, context }) => {
-//   try {
-//     const result = await this.boltClient.client.chat.postEphemeral({
-//       token: context.botToken,
-//       channel: event.channel,
-//       user: event.user,
-//       text: '',
-//       blocks: [
-//         {
-//           type: "section",
-//           text: {
-//             type: "mrkdwn",
-//             text: `<@${message.user}>, I was alerted that you need medical attention when you said, "ow".\nOn a scale of 1 to ten, how would you rate your pain?!`
-//           }
-//         },
-//         {
-//           type: "actions",
-//           elements: [
-//             {
-//               type: "button",
-//               text: {
-//                 type: "plain_text",
-//                 text: ":grinning:",
-//                 emoji: true
-//               },
-//               action_id: "level1",
-//             },
-//             {
-//               type: "button",
-//               text: {
-//                 type: "plain_text",
-//                 text: ":yum:",
-//                 emoji: true
-//               },
-//               action_id: "level2",
-//             },
-//             {
-//               type: "button",
-//               text: {
-//                 type: "plain_text",
-//                 text: ":slightly_smiling_face:",
-//                 emoji: true
-//               },
-//               action_id: "level3",
-//             },
-//             {
-//               type: "button",
-//               text: {
-//                 type: "plain_text",
-//                 text: ":neutral_face:",
-//                 emoji: true
-//               },
-//               action_id: "level4",
-//             },
-//             {
-//               type: "button",
-//               text: {
-//                 type: "plain_text",
-//                 text: ":no_mouth:",
-//                 emoji: true
-//               },
-//               action_id: "level5",
-//             },
-//           ]
-//         },
-//         {
-//           type: "actions",
-//           elements: [
-//             {
-//               type: "button",
-//               text: {
-//                 type: "plain_text",
-//                 text: ":slightly_frowning_face:",
-//                 emoji: true
-//               },
-//               action_id: "level6",
-//             },
-//             {
-//               type: "button",
-//               text: {
-//                 type: "plain_text",
-//                 text: ":white_frowning_face:",
-//                 emoji: true
-//               },
-//               action_id: "level7",
-//             },
-//             {
-//               type: "button",
-//               text: {
-//                 type: "plain_text",
-//                 text: ":anguished:",
-//                 emoji: true
-//               },
-//               action_id: "level8",
-//             },
-//             {
-//               type: "button",
-//               text: {
-//                 type: "plain_text",
-//                 text: ":tired_face:",
-//                 emoji: true
-//               },
-//               action_id: "level9",
-//             },
-//             {
-//               type: "button",
-//               text: {
-//                 type: "plain_text",
-//                 text: ":face_with_symbols_on_mouth:",
-//                 emoji: true
-//               },
-//               action_id: "level10",
-//             },
-//           ]
-//         }]
-//     });
-//     console.log(result);
-//   }
-//   catch (error) {
-//     console.error(error);
-//   }
-// });
-
 export const baymax = new BaymaxBot();
-
-
-
-
